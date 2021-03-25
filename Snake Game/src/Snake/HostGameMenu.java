fc1b0cf14735dc30c46374022d526d416e972127
Keanu Medved <keanu.medved@gmail.com>
2021-03-25 04:04:03 -0700

    improved menu

--- Snake Game/src/Snake/HostGameMenu.java
+++ Snake Game/src/Snake/HostGameMenu.java
@@ -5,11 +5,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class HostGameMenu extends Menu implements ActionListener {
+class HostGameMenu extends Menu implements ActionListener{
 
     //declare variables
+    private final Network network = new Network();
+
     private final GuiManager guiManager;
     private final JPanel hostGamePanel = new JPanel();
+    private final JLabel portNumLabel = new JLabel();
+    private final JTextField portNumBox = new JTextField();
+    private final JButton hostGameButton = new JButton();
     private final JButton backBtn = new JButton();
 
     HostGameMenu(GuiManager guiManager) {
@@ -25,6 +30,9 @@ class HostGameMenu extends Menu implements ActionListener {
         //add components
         JLabel title = new JLabel();
         hostGamePanel.add(title);
+        hostGamePanel.add(portNumLabel);
+        hostGamePanel.add(portNumBox);
+        hostGamePanel.add(hostGameButton);
         hostGamePanel.add(backBtn);
 
         //title
@@ -34,6 +42,23 @@ class HostGameMenu extends Menu implements ActionListener {
         title.setBounds(GameManager.WINDOW_WIDTH / 2 - GuiManager.BUTTON_WIDTH / 2 - 50, 175, 350, 50);
         title.setFont(new Font("TimesNewRoman", Font.BOLD, 64));
         title.setVisible(true);
+
+        //portNumLabel
+        portNumLabel.setText("Port: ");
+        portNumLabel.setBounds(GameManager.WINDOW_WIDTH / 2 - GuiManager.BUTTON_WIDTH / 2 - 25, 350, GuiManager.BUTTON_WIDTH, GuiManager.BUTTON_HEIGHT);
+        portNumLabel.setFont(new Font("TimesNewRoman", Font.PLAIN, 24));
+
+        //portNumBox
+        portNumBox.setText("27100");
+        portNumBox.setBounds(GameManager.WINDOW_WIDTH / 2 - GuiManager.BUTTON_WIDTH / 2 + 50, 360, GuiManager.BUTTON_WIDTH, GuiManager.BUTTON_HEIGHT);
+        portNumBox.setFont(new Font("TimesNewRoman", Font.PLAIN, 24));
+        portNumBox.setSize(100, 30);
+
+        //hostGameButton
+        hostGameButton.setBounds(GameManager.WINDOW_WIDTH / 2 - GuiManager.BUTTON_WIDTH / 2 + 160, 360, 75, 30);
+        hostGameButton.setText("Host");
+        hostGameButton.setFocusable(false);
+        hostGameButton.addActionListener(this);
 
         //back button
         backBtn.setBounds(GameManager.WINDOW_WIDTH / 2 - GuiManager.BUTTON_WIDTH / 2, 700, GuiManager.BUTTON_WIDTH, GuiManager.BUTTON_HEIGHT);
@@ -52,6 +77,9 @@ class HostGameMenu extends Menu implements ActionListener {
     @Override
     public void hideMenu() {
         hostGamePanel.setVisible(false);
+        network.closeLobby();
+        portNumBox.setText("27100");
+
     }
 
     //button actions
@@ -60,6 +88,16 @@ class HostGameMenu extends Menu implements ActionListener {
         if (e.getSource() == backBtn) {
             hideMenu();
             guiManager.getMainMenu().showMenu();
+        }
+        else if (e.getSource() == hostGameButton)
+        {
+            try {
+                network.openLobby(Integer.parseInt(portNumBox.getText()));
+                System.out.println("Server port set to " + Integer.parseInt(portNumBox.getText()));
+            }
+            catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
         }
     }
 }
